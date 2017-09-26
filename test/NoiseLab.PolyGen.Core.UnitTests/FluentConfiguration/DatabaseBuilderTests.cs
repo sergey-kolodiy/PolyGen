4421b2bbbd7c72722b59e76259851bba397f7cb7
Sergey Kolodiy <sergey.kolodiy@gmail.com>
2017-09-26 20:01:17 +0300

    Initial unit tests for fluent DB configuration

---
+++ test/NoiseLab.PolyGen.Core.UnitTests/FluentConfiguration/DatabaseBuilderTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NoiseLab.PolyGen.Core.FluentConfiguration;
+using Xunit;
+
+namespace NoiseLab.PolyGen.Core.UnitTests.FluentConfiguration
+{
+    public class DatabaseBuilderTests
+    {
+        [Fact]
+        public void Table_SchemaIsNull_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => DatabaseBuilder
+                .Create()
+                .Table(null, "Test"));
+        }
+
+        [Fact]
+        public void Table_NameIsNull_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => DatabaseBuilder
+                .Create()
+                .Table("Test", null));
+        }
+
+        [Fact]
+        public void Table_SchemaDoesNotMatchDefaultPattern_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => DatabaseBuilder
+                .Create()
+                .Table("1Test", "Test"));
+        }
+
+        [Fact]
+        public void Table_NameDoesNotMatchDefaultPattern_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => DatabaseBuilder
+                .Create()
+                .Table("Test", "1Test"));
+        }
+
+        [Fact]
+        public void Table_ValidArguments_DoesNotThrow()
+        {
+            // Arrange
+            // Act & Assert
+            DatabaseBuilder
+                .Create()
+                .Table("Test", "Test");
+        }
+
+        [Fact]
+        public void Table_TableAlreadyExists_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => DatabaseBuilder
+                .Create()
+                .Table("Test", "Test")
+                    .Column("Test").String()
+                .Table("Test", "Test"));
+        }
+    }
+}
---
+++ test/NoiseLab.PolyGen.Core.UnitTests/FluentConfiguration/TableBuilderTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NoiseLab.PolyGen.Core.FluentConfiguration;
+using Xunit;
+
+namespace NoiseLab.PolyGen.Core.UnitTests.FluentConfiguration
+{
+    public class TableBuilderTests
+    {
+        [Fact]
+        public void Column_NameIsNull_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => DatabaseBuilder
+                .Create()
+                .Table("Test", "Test")
+                    .Column(null));
+        }
+
+        [Fact]
+        public void Column_NameDoesNotMatchDefaultPattern_ThrowsArgumentException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => DatabaseBuilder
+                .Create()
+                .Table("Test", "Test")
+                    .Column("1Test"));
+        }
+
+        [Fact]
+        public void Column_ColumnAlreadyExists_ThrowsInvalidOperationException()
+        {
+            // Arrange
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => DatabaseBuilder
+                .Create()
+                .Table("Test", "Test")
+                    .Column("Test").String()
+                    .Column("Test"));
+        }
+    }
+}
